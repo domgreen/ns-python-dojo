75e809f30827dfee055a82a7df9613a87f9246be
Dom Green <mail@domgreen.com>
2024-02-08 16:10:43 +0000

    test(roman): adding II

--- roman.py
+++ roman.py
@@ -1,2 +1,8 @@
 def to_decimal(roman_number):
+    if roman_number == "I":
+        return 1
+    
+    if roman_number == "II":
+        return 2
+    
     return 1
--- test_roman.py
+++ test_roman.py
@@ -2,3 +2,6 @@ import roman
 
 def test_I():
     assert roman.to_decimal("I") == 1
+
+def test_II():
+    assert roman.to_decimal("II") == 2
