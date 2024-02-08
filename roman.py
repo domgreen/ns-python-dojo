17c00ce03c8afdd76e648c7698aa338799ff5038
Dom Green <mail@domgreen.com>
2024-02-08 16:30:08 +0000

    test(roman): adding more tests

--- roman.py
+++ roman.py
@@ -1,12 +1,26 @@
-def to_decimal(roman_number):
-    
+def to_decimal(roman_number: str) -> int:
+    """
+    Takes a roman number and returns its decimal value
+    """
     result = 0
-    match roman_number:
-        case "I":
-            result = 1
-        case "II":
-            result = 2
-        case "V":
-            result = 5
-    
+    prev_number = 0
+    for char in roman_number:
+        match char:
+            case "I":
+                result += 1
+                prev_number = 1
+            case "V":
+                if prev_number == 1:
+                   result -= 2
+                result = result + 5
+            case "X":
+                result += 10
+            case "L":
+                result += 50
+            case "C":
+                result += 100
+            case "D":
+                result += 500
+            case "M":
+                result += 1000
     return result
--- test_roman.py
+++ test_roman.py
@@ -6,5 +6,29 @@ def test_I():
 def test_II():
     assert roman.to_decimal("II") == 2
 
+def test_III():
+    assert roman.to_decimal("III") == 3
+
+def test_IV():
+    assert roman.to_decimal("IV") == 4
+
 def test_V():
     assert roman.to_decimal("V") == 5
+
+def test_VI():
+    assert roman.to_decimal("VI") == 6
+
+def test_L():
+    assert roman.to_decimal("L") == 50
+
+def test_X():
+    assert roman.to_decimal("X") == 10
+
+def test_C():
+    assert roman.to_decimal("C") == 100
+
+def test_M():
+    assert roman.to_decimal("M") == 1000
+
+# def test_IX():
+#     assert roman.to_decimal("IX") == 9
